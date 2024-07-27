ded5bc0e072c2a0547cde8fdbdfff536c03f824e
zero <kkenzumi@gmail.com>
2024-07-27 20:30:21 +0700

    setup

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1 className="text-white">Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import { LoginButton } from "@/components/auth/login-button";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1>Next Auth</h1>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center p-24 bg-slate-950">
+      <div className="space-y-6 text-center">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-white text-lg ">Next Auth v5</p>
+        <div>
+          <LoginButton>
+            <Button size="lg" className="bg-lime-600 hover:bg-lime-800">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
