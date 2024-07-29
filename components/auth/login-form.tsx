e2c40499c5eecac5195524b931c7d454a6545030
zero <kkenzumi@gmail.com>
2024-07-29 13:20:28 +0700

    add-CardWrapper

---
+++ components/auth/login-form.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { CardWrapper } from "./card-wrapper";
+
+export const LoginForm = () => {
+  return (
+    <CardWrapper
+      headerLabel="Welcome Back"
+      backButtonHref="/auth/register"
+      backButtonLabel="Don't have an account?"
+      showSocial
+    >
+      Login Form
+    </CardWrapper>
+  );
+};
