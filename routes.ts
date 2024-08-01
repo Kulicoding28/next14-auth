e8860f3f4ff775d58312276ed2678fdf867bb89c
zero <kkenzumi@gmail.com>
2024-08-01 14:04:32 -0700

    feat: settup-middleware

---
+++ routes.ts
@@ -0,0 +1,15 @@
+// **
+// * an array of routes that are accessible to the public
+// *These routes do not require authentication
+// * @type {string[]}
+
+export const publicRoutes = ["/"];
+
+// **
+// * an array of routes that are used for authentication
+// *These routes will redirect logged in users to/settings
+// * @type {string[]}
+
+export const authRoutes = ["auth/login", "auth/register"];
+
+export const apiAuthPrefix = "/api/auth";
