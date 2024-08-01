ea42bd31fd36e6d632f6b3b050b7bc90f3161887
zero <kkenzumi@gmail.com>
2024-08-01 11:16:21 -0700

    feat:make-action-registerForm-bycrypt-Db

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import prisma from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   //   console.log(values);
@@ -10,5 +13,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields.success) {
     return { error: "Invalid fields" };
   }
-  return { succes: "Email sent!" };
+
+  const { name, email, password } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  //   prisma.user.findUnique({
+  //   where: {
+  //     email,
+  //   },
+  // });
+
+  if (existingUser) {
+    return { error: "Email already exists!" };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { succes: "User created" };
 };
